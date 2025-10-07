87340f65b96fc9e49fa8f5252cc0aea2fe561bb4
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-06 21:02:42 -0500

    HU-15 Login: Implementar login de usuario con validacion de credenciales

--- backend/statsprime/users/urls.py
+++ backend/statsprime/users/urls.py
@@ -8,4 +8,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
