5e0454122d72087fd2150bb433cd1deca78ea2b7
Alejandro Medina <weaqla.xd@gmail.com>
2025-10-10 16:07:37 -0500

    Revert "Merge pull request #4 from DocPaloma/feature/US-16-Register"
    This reverts commit e2a1400872095d93d00e51b68f7be2d05f768535, reversing
    changes made to 500b807aeadf4a8d7fdd5986525eb154767187c5.

--- backend/statsprime/statsprime/settings.py
+++ backend/statsprime/statsprime/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,30 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'rest_framework',
-    'rest_framework_simplejwt',
 ]
-
-AUTH_USER_MODEL = 'users.User'
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-    ),
-}
-
-load_dotenv(os.path.join(BASE_DIR, '.env'))
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -99,15 +74,10 @@ WSGI_APPLICATION = 'statsprime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'statsprime_db',      # nombre de tu base de datos
-        'USER': 'postgres',           # tu usuario de PostgreSQL
-        'PASSWORD': 'admin123',  # tu contraseña
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
