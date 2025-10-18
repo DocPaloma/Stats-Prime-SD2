e50c3163af03f403d102ac7c3b35ef9337d71388
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-18 11:43:00 -0500

    fix: restauracion del backend tras error en la version anterior

--- backend/statsprime/users/urls.py
+++ backend/statsprime/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import (
-    RegisterView, PasswordResetEmailView, PasswordResetConfirmView,
-    PasswordResetBySecretView, ProfileView, ChangePasswordView
+    RegisterView, PasswordResetBySecretView, ProfileView
 )
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -12,8 +11,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('change-password/', ChangePasswordView.as_view(), name='change_password'),  # <- NUEVA
-    path('password-reset-email/', PasswordResetEmailView.as_view(), name='password_reset_email'),
-    path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password-reset-secret/', PasswordResetBySecretView.as_view(), name='password_reset_secret'),
 ]
